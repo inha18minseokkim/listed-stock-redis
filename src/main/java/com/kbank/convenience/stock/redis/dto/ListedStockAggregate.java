fe5f095e78ee923e543f58d051ec83f60c72a507
inha18minseokkim <bjm7701@naver.com>
2024-09-10 12:10:48 +0900

    ListedStockAggregate 추가

---
+++ src/main/java/com/kbank/convenience/stock/redis/dto/ListedStockAggregate.java
@@ -0,0 +1,27 @@
+package com.kbank.convenience.stock.redis.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class ListedStockAggregate implements Serializable {
+    private String symbol;
+    private String entity_name;
+    private Double open;
+    private Double high;
+    private Double low;
+    private Long volume;
+    private Double change;
+    private Double value;
+    private Double change_percent;
+    private Boolean trading_halted;
+    private Double market_cap;
+    private Long shares_listed;
+    private Double close;
+}
