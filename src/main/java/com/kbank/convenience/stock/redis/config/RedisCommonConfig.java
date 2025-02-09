8a9754471ce047b67bb9c1fb948171341c082567
kimminseok <bjm7701@naver.com>
2025-02-09 15:35:48 +0900

    boot 종속성 제거

--- src/main/java/com/kbank/convenience/stock/redis/config/RedisCommonConfig.java
+++ src/main/java/com/kbank/convenience/stock/redis/config/RedisCommonConfig.java
@@ -6,11 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -26,7 +31,7 @@ public class RedisCommonConfig {
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
-    @Bean
+    @Bean("redisTemplate")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 
         RedisTemplate<String, Object> template = new RedisTemplate<>();
@@ -43,5 +48,23 @@ public class RedisCommonConfig {
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(genericJackson2JsonRedisSerializer);
         return template;
+    }
+    @Bean
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+    @Bean("reactiveRedisTemplate")
+    public ReactiveRedisOperations<String, Object> reactiveRedisTemplate() {
+        ReactiveRedisConnectionFactory rrcf = reactiveRedisConnectionFactory();
+
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Object> builder = RedisSerializationContext
+                .newSerializationContext(new StringRedisSerializer());
+
+        RedisSerializationContext<String, Object> context = builder.value(serializer).hashValue(serializer)
+                .hashKey(serializer).build();
+
+        return new ReactiveRedisTemplate<>(rrcf, context);
     }
 }
